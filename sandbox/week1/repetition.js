f7a0e41b094bbad2b508be50a3208047219c60ff
krichardson-tech <kali.r.4115@gmail.com>
2026-09-18 11:44:46 -0600

    activity 2: js variables and constants - ponder

---
+++ sandbox/week1/repetition.js
@@ -0,0 +1,9 @@
+//Javascript example 2
+
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples;
+
+document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("friendAppleElement").textContent = friendApples;
+document.getElementById("totalApplesElement").textContent = total;
