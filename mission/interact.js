cab0983e32a67425cd1fba88c8fa09534a74c9ee
krichardson-tech <kali.r.4115@gmail.com>
2026-09-25 16:03:22 -0600

    1st attempt at dark mode (isn't working)

--- mission/interact.js
+++ mission/interact.js
@@ -7,8 +7,14 @@ function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
         // code for changes to colors and logo
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+        logo.src = 'images/byui-logo-white.png';
     } else {
         // code for changes to colors and logo
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+        logo.src = 'images/byui-logo-blue.webp';
     }
 }           
                     
